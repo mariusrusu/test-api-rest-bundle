82d7769cf67d30bd920f98de35210e23c4dec6b2
Edenn Touitou <deozza@gmail.com>
2019-01-08 12:11:22 +0100

    still fixing namespaces

--- Tests/sampleProject/app/DataFixtures/ORM/LoadDemoFixture.php
+++ Tests/sampleProject/app/DataFixtures/ORM/LoadDemoFixture.php
@@ -1,19 +1,24 @@
 <?php
 
-namespace EveryCheck\TestApiRestBundle\Tests\sampleProject\tests\SampleProjectBisBundle\DataFixtures\ORM;
+namespace EveryCheck\TestApiRestBundle\Tests\sampleProject\app\DataFixtures\ORM;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use EveryCheck\TestApiRestBundle\Tests\sampleProject\src\SampleProjectBundle\Entity\Demo;
 
-class LoadDemoBisFixture extends Fixture
+class LoadDemoFixture extends Fixture
 {
     public function load(ObjectManager $manager)
     {
         $demo1 = new Demo();
-        $demo1->setName("bis");
-        $demo1->setValue(312);
+        $demo1->setName("foo");
+        $demo1->setValue(300);
         $manager->persist($demo1);
+
+        $demo2= new Demo();
+        $demo2->setName("the answer");
+        $demo2->setValue(42);
+        $manager->persist($demo2);
 
         $manager->flush();
     }
